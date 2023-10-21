509dd14862e2801a7d79f727b69b70d04eac0f8f
Anurudhan <147535668+Anurudhan@users.noreply.github.com>
2023-10-21 09:40:24 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "40%";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
+/*dropdown*/
+  function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown-content");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
